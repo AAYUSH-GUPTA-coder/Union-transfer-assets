b8d3d5b6cbc550796f4fce26f9cca9e04d5f09e0
Aayush Gupta <guptaaayush432@gmail.com>
2024-10-18 22:11:09 +0530

    added parameter

--- script/CallTransfer.s.sol
+++ script/CallTransfer.s.sol
@@ -6,13 +6,17 @@ import {Transfer} from "../src/Transfer.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 interface ITransfer {
-    function transferAsset() external;
+    function transferAsset(bytes calldata _receiver) external;
 }
 
 contract CallTransferScript is Script {
     address public constant USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
+    // address public constant TRANSFER_ADDR =
+    //     0x3cA600DEEE77892c738843F02FaF627Ab162f5a8;
     address public constant TRANSFER_ADDR =
-        0x3cA600DEEE77892c738843F02FaF627Ab162f5a8;
+        0x8eb0fD3B3fb1bF44CF7C01785c74a7A8dCD6E60C;
+    bytes public constant RECEIVER =
+        hex"c747c491a442191646a349570bbf33b36a0cc1a9";
 
     function run() public {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
@@ -22,7 +26,7 @@ contract CallTransferScript is Script {
         IERC20(USDC).transfer(TRANSFER_ADDR, 1500000);
 
         console.log("transferring");
-        ITransfer(TRANSFER_ADDR).transferAsset();
+        ITransfer(TRANSFER_ADDR).transferAsset(RECEIVER);
         console.log("complete");
         vm.stopBroadcast();
     }
